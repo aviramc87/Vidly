949199f171c78142da8f6e217a700f4fcca6951a
aviramc87 <aviramc87@gmail.com>
2018-04-02 21:32:00 +0300

    kj

--- Vidly/Controllers/HomeController.cs
+++ Vidly/Controllers/HomeController.cs
@@ -16,7 +16,7 @@ namespace Vidly.Controllers
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
-
+            //kpajsdlajsdlakjshdkajsdhkajshkajhds
             return View();
         }
 
